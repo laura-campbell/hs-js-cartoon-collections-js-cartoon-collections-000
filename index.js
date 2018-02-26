745c15c644c6b237dff819003f058c40604b1fc2
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-26 16:37:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+var string='';
 function dwarfRollCall(dwarves) {
+  for (var i=0; i<dwarves.length; i++) {
+  string += i+1 + '. ' + dwarves[i] + ' ';
+}
+  return string;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  for (var i=0; i<planeteerCalls.length; i++) {
+  planeteerCalls[i] = planeteerCalls[i].toUpperCase() + '!';
+  }
+  return planeteerCalls;
 }
 
 function longPlaneteerCalls(words) {
+ var boo='';
+ for (var i=0; i<words.length; i++) {
+ boo += Boolean (words[i].length > 4)}
+ return Boolean (boo.indexOf(true) !== -1);
 }
 
 function findTheCheese (foods) {
-}
+  var cheese = ['cheddar', 'gouda', 'camembert'];
+  var string;
+  for (var i=0; i<foods.length; i++) { 
+    string = cheese.indexOf(foods[i]);
+    if (string !== -1) {
+      return foods[i];
+    }}
+    return 'no cheese!';
+  }
